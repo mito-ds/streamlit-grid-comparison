63071fbc1dad02d049e3d95644fdd811d8d5cbac
Nate Rush <naterush1997@gmail.com>
2023-09-12 14:06:01 -0700

    update path

--- Grid_Comparison.py
+++ Grid_Comparison.py
@@ -6,13 +6,13 @@ st.markdown("""
             
 ## Motivation
             
-Streamlit makes it easy to build and share data apps with your teammates. 
+Streamlit makes it easy to share data apps with your team. But as the last 40 years of Excel's dominance has shown, your teammates want a grid!
             
-Of course, your teammates want a grid! At least, so says the past 40 years of Excel’s dominance.
-
-Streamlit has a variety of built-in grid components, and there are a variety of third party grids as well.
+Streamlit has a variety of built-in grid components, and there are a variety of third party libraries that provide grids as well. 
             
-This app, explores the most popular Streamlit grids, and discusses which are appropriate for your use case.
+This app is designed to help you choose the right grid for your app. To use this app:
+1. Explore the different grids in the sidebar
+2. Use the Which Grid is Right for Me? section to help you choose the right grid for your app
 
 ## The Contenders
             
@@ -25,8 +25,7 @@ This app, explores the most popular Streamlit grids, and discusses which are app
 
 #### A disclaimer  
 
-We’re the authors of the `Mito` package — but we’re doing our very best to stay unbiased! Mito is great for some use cases, but not others. 
+We’re the authors of the `Mito` package — but we’re doing our very best to stay unbiased! Mito is great for some use cases, but isn't a great fit for others. 
             
 Feel free to open a PR against this [repo](https://github.com/mito-ds/streamlit-grid-comparison) if you think we can represent any of the libraries here better.
-
 """)
